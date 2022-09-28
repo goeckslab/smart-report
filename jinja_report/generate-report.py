7cb059f4ed63c14b7fa6ea2ee78613fd5e140949
qiagu <guqiang01@gmail.com>
2022-09-27 23:32:14 -0700

    Initiate jinja report

---
+++ jinja_report/generate-report.py
@@ -0,0 +1,52 @@
+"""
+Supported data types are:
+PNG, JSON, HTML, Others as binary.
+"""
+import pathlib
+import os
+
+from collections import defaultdict
+from datetime import datetime
+
+import jinja2
+import yaml
+
+
+REPORT_INPUTS_PATH = pathlib.Path("report_input.yml")
+DATA_PATH = "data"
+TEMPLATE_PATH = "templates"
+STATIC_PATH = "static"
+
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader([TEMPLATE_PATH, STATIC_PATH]),
+)
+
+if REPORT_INPUTS_PATH.exists():
+    with open(REPORT_INPUTS_PATH, "r") as fh:
+        inputs = yaml.safe_load(fh)
+else:
+    inputs = defaultdict(list)
+    for fl in sorted(os.listdir(DATA_PATH), key=lambda e: e.lower()):
+        element =  {"src": DATA_PATH+"/"+fl, "label": fl[:-4]}
+        if fl.endswith(".png"):
+            inputs["image"].append({"id": "image_"+str(len(inputs["image"])), **element})
+        elif fl.endswith(".json"):
+            inputs["json"].append({"id": "json_"+str(len(inputs["json"])), **element})
+        elif fl.endswith(".html"):
+            inputs["html"].append({"id": "html_"+str(len(inputs["html"])), **element})
+        else:
+            inputs["others"].append({"id": "others_"+str(len(inputs["others"])), **element})
+
+if not inputs.get('title'):
+    inputs["title"] = "Smart Report"
+
+inputs["report_time"] = datetime.now()
+
+# print(dict(inputs))
+
+main_tpl = env.get_template("main.html")
+
+out = main_tpl.render(inputs=dict(inputs))
+
+print(out)
